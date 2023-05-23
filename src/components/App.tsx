9a881d2a865de63a1c0e1b6d7e6ced42673fff0c
Vitor Aderaldo <vitor.aldero@harmonize.health>
2023-05-23 00:07:07 -0300

    feat: set up project

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
