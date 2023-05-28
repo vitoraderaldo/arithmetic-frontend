47dbf1807a3249040a25703006c91edef8ebad62
Vitor Aderaldo <vitor.aldero@harmonize.health>
2023-05-27 22:26:13 -0300

    feat: logout user

--- src/components/left-menu.tsx
+++ src/components/left-menu.tsx
@@ -1,33 +1,56 @@
-import { Drawer, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { Box, Drawer, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import HistoryIcon from '@mui/icons-material/History';
 import { NavLink, useMatch } from "react-router-dom";
 import CalculateIcon from "@mui/icons-material/Calculate";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { LogoutDialog } from "./logout-dialog";
+import { Fragment, useState } from "react";
 
 export const LeftMenu = () => {
   const isHome = !!useMatch('/');
   const isRecords = !!useMatch('/records');
+
+  const [showLogout, setShowLogout] = useState(false);
+
   return (
-    <Drawer variant="permanent" anchor="left" PaperProps={{
-      sx: {
-        position: 'static',
-        height: '100vh',
-        width: '100%'
-      }
-    }}>
-      <List>
-        <ListItemButton to="/" component={NavLink} selected={isHome}>
-          <ListItemIcon>
-            <CalculateIcon   />
-          </ListItemIcon>
-          <ListItemText primary="Calculator" />
-        </ListItemButton>
-        <ListItemButton to="/records" component={NavLink} selected={isRecords}>
-          <ListItemIcon>
-            <HistoryIcon />
-          </ListItemIcon>
-          <ListItemText primary="Records" />
-        </ListItemButton>
-      </List>
-    </Drawer>
+    <Fragment>
+      <Drawer variant="permanent" anchor="left" PaperProps={{
+        sx: {
+          position: 'static',
+          height: '100vh',
+          width: '100%'
+        }
+      }}>
+
+        <Box height="100%" display="flex" flexDirection="column" justifyContent="space-between">
+          <List>
+            <ListItemButton to="/" component={NavLink} selected={isHome}>
+              <ListItemIcon>
+                <CalculateIcon   />
+              </ListItemIcon>
+              <ListItemText primary="Calculator" />
+            </ListItemButton>
+            <ListItemButton to="/records" component={NavLink} selected={isRecords}>
+              <ListItemIcon>
+                <HistoryIcon />
+              </ListItemIcon>
+              <ListItemText primary="Records" />
+            </ListItemButton>
+          </List>
+          <List style={{borderTop: '1px solid #0000001F'}}>
+            <ListItemButton onClick={() => setShowLogout(true)} >
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary="Logout" />
+            </ListItemButton>
+          </List>
+        </Box>
+      </Drawer>
+      <LogoutDialog 
+        isOpened={showLogout}
+        onCancel={() => setShowLogout(false)}
+      />
+    </Fragment>
   );
 };
---
+++ src/components/logout-dialog.tsx
@@ -0,0 +1,34 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import { logoutUser } from "../util/auth/authentication.util";
+
+interface LogoutDialogProps {
+  isOpened: boolean;
+  onCancel: () => void;
+}
+
+export const LogoutDialog = (props: LogoutDialogProps) => {
+
+  const { 
+    isOpened, 
+    onCancel, 
+  } = props;
+
+  return (
+    <Dialog open={isOpened} onClose={onCancel} maxWidth="xs">
+      <DialogTitle>Confirm Logout</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Are you sure you want to log out?
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onCancel} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={logoutUser} color="primary">
+          Logout
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
